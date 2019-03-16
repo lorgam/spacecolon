4c1e0df7a6ec5c4b0cde55bc37d2fc386d8a7192
Luis <lorgam@gmail.com>
2019-03-16 14:53:59 +0100

    Added map & map tiles

--- gameScreen.js
+++ gameScreen.js
@@ -5,6 +5,15 @@ function GameScreen(context){
 	this.totalTime		= 0.0;
 	this.context		= context;
 
+	this.map			= new Array(mapOptions.width);
+	var w, h;
+	for (w = 0; w < mapOptions.width; w++){
+		this.map[w] = new Array(mapOptions.height);
+		for (h = 0; h < mapOptions.height; h++){
+			this.map[w][h] = new MapTile(w+h);
+		}
+	}
+	console.log(this.map);
 	this.update = function(timeElapsed){
 		this.totalTime += timeElapsed;
 
@@ -21,3 +30,13 @@ function GameScreen(context){
 }
 
 export default GameScreen;
+
+var colors = ["0000FF","00FF00","FF0000"];
+var mapOptions = {
+	width	: 10,
+	height	: 10
+};
+
+function MapTile(seed){
+	this.color = seed % colors.length;
+}
