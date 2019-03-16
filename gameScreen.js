57dfc60efc23d321c69f37a3350595bef7905465
Luis <lorgam@gmail.com>
2019-03-16 16:41:47 +0100

    Added a world map class

--- gameScreen.js
+++ gameScreen.js
@@ -1,42 +1,31 @@
 import GLOBALS	from './globals.js';
 import INPUT	from './input.js';
+import WorldMap	from './world_map/worldMap.js';
 
 function GameScreen(context){
-	this.totalTime		= 0.0;
-	this.context		= context;
+	this.totalTime				= 0.0;
+	this.context				= context;
+	this.map					= new WorldMap(20,16);
 
-	this.map			= new Array(mapOptions.width);
-	var w, h;
-	for (w = 0; w < mapOptions.width; w++){
-		this.map[w] = new Array(mapOptions.height);
-		for (h = 0; h < mapOptions.height; h++){
-			this.map[w][h] = new MapTile(w+h);
-		}
-	}
-	console.log(this.map);
 	this.update = function(timeElapsed){
 		this.totalTime += timeElapsed;
 
 		if (INPUT.keyboard.ESC.execute(timeElapsed)) {
 			GLOBALS.screenStack.shift();
 		}
+		if (INPUT.keyboard.ARROW_LEFT.execute(timeElapsed))		this.map.moveLeft();
+		if (INPUT.keyboard.ARROW_RIGHT.execute(timeElapsed))	this.map.moveRight();
+		if (INPUT.keyboard.ARROW_UP.execute(timeElapsed))		this.map.moveUp();
+		if (INPUT.keyboard.ARROW_DOWN.execute(timeElapsed))		this.map.moveDown();
 
 	}
 
 	this.draw = function(){
-		this.context.fillStyle	= "#00FFFF";
+		this.context.fillStyle	= "#000000";
 		this.context.fillRect(0, 0, GLOBALS.width, GLOBALS.height);
+
+		this.map.draw(this.context);
 	}
 }
 
 export default GameScreen;
-
-var colors = ["0000FF","00FF00","FF0000"];
-var mapOptions = {
-	width	: 10,
-	height	: 10
-};
-
-function MapTile(seed){
-	this.color = seed % colors.length;
-}
---
+++ world_map/worldMap.js
@@ -0,0 +1,47 @@
+function WorldMap(width, height){
+	this.width		= width;
+	this.height		= height;
+	//Tile in the top-left corner of the userview
+	this.topLeftX	= 0;
+	this.topLeftY	= 0;
+	//Generate map
+	this.map		= new Array(this.width);
+
+	for (var w = 0; w < this.width; w++){
+		this.map[w] = new Array(this.height);
+
+		for (var h = 0; h < this.height; h++){
+			this.map[w][h] = new MapTile(w+h);
+		}
+	}
+}
+
+WorldMap.prototype.draw = function(context){
+	var horizontalTilesToShow	= screenSize.width  / tileSize;
+	var verticalTilesToShow		= screenSize.height / tileSize;
+
+	var w, h, mapTile;
+	for (w = 0; w < horizontalTilesToShow; w++){
+		for (h = 0; h < verticalTilesToShow; h++){
+			mapTile = this.map[(this.topLeftX + w) % this.width][(this.topLeftY + h) % this.height];
+
+			context.fillStyle	= colors[mapTile.color];
+			context.fillRect(w * tileSize, h * tileSize, tileSize, tileSize);
+		}
+	}
+}
+
+WorldMap.prototype.moveLeft		= function(){this.topLeftX = (this.width + this.topLeftX - 1) % this.width;}
+WorldMap.prototype.moveRight	= function(){this.topLeftX = (this.topLeftX + 1) % this.width;}
+WorldMap.prototype.moveUp		= function(){this.topLeftY = (this.height + this.topLeftY - 1) % this.height;}
+WorldMap.prototype.moveDown		= function(){this.topLeftY = (this.topLeftY + 1) % this.height;}
+
+export default WorldMap;
+
+var screenSize	= {width:800,height:600};
+var tileSize	= 40;
+
+function MapTile(seed){
+	this.color = seed % colors.length;
+}
+var colors = ["#0000FF","#00FF00","#FF0000","#FF00FF"];
