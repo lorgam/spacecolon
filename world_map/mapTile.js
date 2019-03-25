7b7fcbfb10c8427359c8d694bcd816deb135e899
Luis <lorgam@gmail.com>
2019-03-25 12:30:01 +0100

    Changed colors & fixed parameters for terrain

--- world_map/mapTile.js
+++ world_map/mapTile.js
@@ -7,35 +7,37 @@ function MapTile(parent, heightSeed, humiditySeed){
 }
 
 MapTile.prototype.tileTypes = {
-	"deepOcean"			: {"color":"#0000FF"},
+	"deepOcean"			: {"color":"#000044"},
 	"ocean"				: {"color":"#000088"},
-	"beach"				: {"color":"#444400"},
+	"shallowWaters"		: {"color":"#0000FF"},
+	"beach"				: {"color":"#FFFF00"},
 	"highMountain"		: {"color":"#FFFFFF"},
 	"desert"			: {"color":"#888800"},
-	"grass"				: {"color":"#880000"},
-	"forest"			: {"color":"#FF0000"},
-	"desertMountain"	: {"color":"#FFFF00"},
-	"grassMountain"		: {"color":"#880088"},
-	"forestMountain"	: {"color":"#FF00FF"},
+	"grass"				: {"color":"#00FF00"},
+	"forest"			: {"color":"#008800"},
+	"desertMountain"	: {"color":"#444400"},
+	"grassMountain"		: {"color":"#FF00FF"},
+	"forestMountain"	: {"color":"#880088"},
 }
 
 MapTile.prototype.tileType = function(waterHeight){
 	var waterHeight = this.parent.waterHeight;
 	if (this.height < waterHeight - 0.25)	return "deepOcean";
-	if (this.height < waterHeight)			return "ocean";
-	if (this.height < waterHeight + 0.05)	return "beach";
+	if (this.height < waterHeight - 0.05)	return "ocean";
+	if (this.height < waterHeight)			return "shallowWaters";
+	if (this.height < waterHeight + 0.02)	return "beach";
 	if (this.height > 0.85)					return "highMountain";
 
 	var heightDifference = 0.8 - waterHeight; //Amount of height left between beach and high mountain
 
 	if (this.height < 0.6 * heightDifference + waterHeight){
 		//Not mountain
-		if (this.humiditySeed < 0)			return "desert";
+		if (this.humiditySeed < -0.4)		return "desert";
 		if (this.humiditySeed < 0.5)		return "grass";
 		return "forest";
 	}
 	//Mountain
-	if (this.humiditySeed < 0)			return "desertMountain";
+	if (this.humiditySeed < -0.4)		return "desertMountain";
 	if (this.humiditySeed < 0.5)		return "grassMountain";
 	return "forestMountain";
 }
