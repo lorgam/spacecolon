9f960a2d2ac9e873dfa46d789f08e4c03e461bb7
Luis <lorgam@gmail.com>
2019-04-01 18:11:31 +0200

    Uniformity

--- globals.js
+++ globals.js
@@ -12,20 +12,19 @@ const GLOBALS = {
 	minTileSize				: 8,
 	maxTileSize				: 32,
 
-	horizontalTilesToShow	: function(){return GLOBALS.mainScreenWidth  / GLOBALS.tileSize;},
-	verticalTilesToShow		: function(){return GLOBALS.mainScreenHeight / GLOBALS.tileSize;},
-	bottomOfMap				: function(){return GLOBALS.topMenuHeight + GLOBALS.mainScreenHeight;},
-	rightMenuSize			: function(){return GLOBALS.width - GLOBALS.mainScreenWidth;},
-	lowerMenuSize			: function(){return GLOBALS.height - GLOBALS.bottomOfMap();},
-
 	menuFont				: "30px Arial",
 	normalFont				: "14px Arial",
-
-	fontHeight				: function(){return parseInt(GLOBALS.context.font.match(/\d+/));}, //Requires the fonts to be specified in px
 
 	backgroundColor			: "#000000",
 	textColor				: "#FFFFFF",
 	highlightColor			: "#FF0000",
 };
+
+GLOBALS.horizontalTilesToShow	= function(){return GLOBALS.mainScreenWidth  / GLOBALS.tileSize;}
+GLOBALS.verticalTilesToShow		= function(){return GLOBALS.mainScreenHeight / GLOBALS.tileSize;}
+GLOBALS.bottomOfMap				= function(){return GLOBALS.topMenuHeight + GLOBALS.mainScreenHeight;}
+GLOBALS.rightMenuSize			= function(){return GLOBALS.width - GLOBALS.mainScreenWidth;}
+GLOBALS.lowerMenuSize			= function(){return GLOBALS.height - GLOBALS.bottomOfMap();}
+GLOBALS.fontHeight				= function(){return parseInt(GLOBALS.context.font.match(/\d+/));} //Requires the fonts to be specified in px
 
 export default GLOBALS;
--- screens/mainMenuScreen.js
+++ screens/mainMenuScreen.js
@@ -7,49 +7,49 @@ function MainMenuScreen(){
 	this.selectedOption	= 0;
 	this.optionArray	= ["Start", "Options", "Exit"];
 	this.padding		= 2;
+}
 
-	this.update = function(timeElapsed){
-		this.totalTime += timeElapsed;
+MainMenuScreen.prototype.update = function(timeElapsed){
+	this.totalTime += timeElapsed;
 
-		if (INPUT.keyboard.ARROW_UP.execute(timeElapsed)) {
-			this.selectedOption = (this.selectedOption + this.optionArray.length - 1) % this.optionArray.length;
-		}
-		if (INPUT.keyboard.ARROW_DOWN.execute(timeElapsed)) {
-			this.selectedOption = (this.selectedOption + 1) % this.optionArray.length;
-		}
-		if (INPUT.keyboard.ENTER.execute(timeElapsed)) {
-			if (this.selectedOption == 0) GLOBALS.screenStack.unshift(new GameScreen());
-		}
+	if (INPUT.keyboard.ARROW_UP.execute(timeElapsed)) {
+		this.selectedOption = (this.selectedOption + this.optionArray.length - 1) % this.optionArray.length;
 	}
+	if (INPUT.keyboard.ARROW_DOWN.execute(timeElapsed)) {
+		this.selectedOption = (this.selectedOption + 1) % this.optionArray.length;
+	}
+	if (INPUT.keyboard.ENTER.execute(timeElapsed)) {
+		if (this.selectedOption == 0) GLOBALS.screenStack.unshift(new GameScreen());
+	}
+}
 
-	this.draw = function(){
-		var context = GLOBALS.context;
-		context.fillStyle	= GLOBALS.backgroundColor;
-		context.fillRect(0, 0, GLOBALS.width, GLOBALS.height);
+MainMenuScreen.prototype.draw = function(){
+	var context = GLOBALS.context;
+	context.fillStyle	= GLOBALS.backgroundColor;
+	context.fillRect(0, 0, GLOBALS.width, GLOBALS.height);
 
-		context.fillStyle	= GLOBALS.textColor;
-		context.font		= GLOBALS.menuFont;
+	context.fillStyle	= GLOBALS.textColor;
+	context.font		= GLOBALS.menuFont;
 
-		var fontHeight			= GLOBALS.fontHeight();
-		var totalHeight			= (fontHeight + (2*this.padding)) * this.optionArray.length;
+	var fontHeight			= GLOBALS.fontHeight();
+	var totalHeight			= (fontHeight + (2*this.padding)) * this.optionArray.length;
 
-		var text, width, top	= ((GLOBALS.height - totalHeight) / 2.0) + this.padding + fontHeight;
-		for (var i in this.optionArray){
-			text	= this.optionArray[i];
-			width	= context.measureText(text).width;
+	var text, width, top	= ((GLOBALS.height - totalHeight) / 2.0) + this.padding + fontHeight;
+	for (var i in this.optionArray){
+		text	= this.optionArray[i];
+		width	= context.measureText(text).width;
 
-			if (this.selectedOption == i){
-				var g	= Math.lerp(0, 128, aux.gradient);
+		if (this.selectedOption == i){
+			var g	= Math.lerp(0, 128, aux.gradient);
 
-				context.fillStyle	= 'rgb(255,'+g+','+g+')';
-				context.fillText(text, (GLOBALS.width - width) / 2, top);
-				context.fillStyle	= GLOBALS.textColor;
-			}
-
-			else context.fillText(text, (GLOBALS.width - width) / 2, top);
-
-			top += (2 * this.padding) + fontHeight;
+			context.fillStyle	= 'rgb(255,'+g+','+g+')';
+			context.fillText(text, (GLOBALS.width - width) / 2, top);
+			context.fillStyle	= GLOBALS.textColor;
 		}
+
+		else context.fillText(text, (GLOBALS.width - width) / 2, top);
+
+		top += (2 * this.padding) + fontHeight;
 	}
 }
 
