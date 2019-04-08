b64bacab6dd6ef22e49fb54b75597c054e15084a
Luis <lorgam@gmail.com>
2019-04-08 16:17:47 +0200

    Generating resources on resources object

--- resources/resources.js
+++ resources/resources.js
@@ -4,3 +4,49 @@ const resources = {
 }
 
 export default resources;
+
+
+resources.generate = function(map){
+	var mapContext	= map.mapCanvas.getContext('2d');
+	var totalSize = map.options.width * map.options.height;
+	var x, y, tile, total;
+	//Generate minerals
+	total = ~~(totalSize  * 0.0015);
+
+	while (total > 0){
+		x = ~~(Math.random() * map.options.width);
+		y = ~~(Math.random() * map.options.height);
+
+		tile = map.map[x][y];
+
+		if (tile.height > map.options.waterHeight + 0.15){
+			//Can have minerals
+			if (tile.resource) continue;
+			total--;
+			tile.resource = resources.MINERAL;
+
+			mapContext.fillStyle = resources.MINERAL.color;
+			mapContext.fillRect(x * GLOBALS.maxTileSize, y * GLOBALS.maxTileSize, GLOBALS.maxTileSize, GLOBALS.maxTileSize);
+		}
+	}
+	//Generate gas
+	total = ~~(totalSize  * 0.0005);
+
+	while (total > 0){
+		x = ~~(Math.random() * map.options.width);
+		y = ~~(Math.random() * map.options.height);
+
+		tile = map.map[x][y];
+
+		if (tile.height < map.options.waterHeight + 0.15){
+			//Can have gas
+			if (tile.resource) continue;
+			total--;
+			tile.resource = resources.GAS;
+
+			mapContext.fillStyle = resources.GAS.color;
+			mapContext.fillRect(x * GLOBALS.maxTileSize, y * GLOBALS.maxTileSize, GLOBALS.maxTileSize, GLOBALS.maxTileSize);
+		}
+	}
+	//Generate life
+}
--- world_map/worldGenerator.js
+++ world_map/worldGenerator.js
@@ -6,7 +6,7 @@ const WorldGenerator ={}
 
 WorldGenerator.generate = function(parent){
 	WorldGenerator.generateWorld(parent);
-	WorldGenerator.generateResources(parent);
+	resources.generate(parent);
 }
 
 WorldGenerator.generateWorld = function(parent){
@@ -59,51 +59,6 @@ WorldGenerator.generateWorld = function(parent){
 
 	parent.map			= map;
 	parent.mapCanvas	= mapCanvas;
-}
-
-WorldGenerator.generateResources = function(parent){
-	var mapContext	= parent.mapCanvas.getContext('2d');
-	var totalSize = parent.options.width * parent.options.height;
-	var x, y, tile, total;
-	//Generate minerals
-	total = ~~(totalSize  * 0.0015);
-
-	while (total > 0){
-		x = ~~(Math.random() * parent.options.width);
-		y = ~~(Math.random() * parent.options.height);
-
-		tile = parent.map[x][y];
-
-		if (tile.height > parent.options.waterHeight + 0.15){
-			//Can have minerals
-			if (tile.resource) continue;
-			total--;
-			tile.resource = resources.MINERAL;
-
-			mapContext.fillStyle = resources.MINERAL.color;
-			mapContext.fillRect(x * GLOBALS.maxTileSize, y * GLOBALS.maxTileSize, GLOBALS.maxTileSize, GLOBALS.maxTileSize);
-		}
-	}
-	//Generate gas
-	total = ~~(totalSize  * 0.0005);
-
-	while (total > 0){
-		x = ~~(Math.random() * parent.options.width);
-		y = ~~(Math.random() * parent.options.height);
-
-		tile = parent.map[x][y];
-
-		if (tile.height < parent.options.waterHeight + 0.15){
-			//Can have gas
-			if (tile.resource) continue;
-			total--;
-			tile.resource = resources.GAS;
-
-			mapContext.fillStyle = resources.GAS.color;
-			mapContext.fillRect(x * GLOBALS.maxTileSize, y * GLOBALS.maxTileSize, GLOBALS.maxTileSize, GLOBALS.maxTileSize);
-		}
-	}
-	//Generate life
 }
 
 WorldGenerator.generateOptions = function(definition){
