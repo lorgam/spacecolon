8d6e18d4c985d0e2522e94e8d0ffe9cb901f7642
Luis <lorgam@gmail.com>
2019-10-02 19:44:25 +0100

    Cleaned textures script

--- src/neuron/textureManager.js
+++ src/neuron/textureManager.js
@@ -7,67 +7,84 @@ import naturalResourceManager	from '../resources/naturalResourceManager.js';
 const textureManager = {
 	textures : [],
 	load : function(){
-		var context, canvas, textures, size = 100;
+		var canvas, textures, size = 100;
+
 		// tiles
 		textures = [];
+
 		for (var tileType in MapTile.prototype.tileTypes) {
-			[canvas, context] = createCanvas(size, MapTile.prototype.tileTypes[tileType].color);
+			canvas = createCanvas(size, size);
+			addBackground(canvas, MapTile.prototype.tileTypes[tileType].color);
 			textures[tileType] = canvas;
 		}
+
 		textureManager.textures['mapTile'] = textures;
+
 		// wildlife
 		textures = [];
+
 		for (var life in lifeManager) {
 			var elem = lifeManager[life];
 			if (elem.color){
-				[canvas, context] = createCanvas(size, elem.color);
+				canvas = createCanvas(size, size);
+				addBackground(canvas, elem.color);
 				textures[life] = canvas;
 			}
 		}
+
 		textureManager.textures['wildLife'] = textures;
+
 		// user resources
 		textures = [];
-		[canvas, context] = createCanvas(size, GLOBALS.backgroundColor);
-		addTriangle(context, size, "#FFFF00");
+
+		canvas = createCanvas(size, size);
+		addTriangle(canvas, size, "#FF0");
 		textures['robots'] = canvas;
 
 		textureManager.textures['userResources'] = textures;
+
 		// natural resource
 		textures = [];
-		[canvas, context] = createCanvas(size, );
 
 		for (var res  in naturalResourceManager) {
 			var elem = naturalResourceManager[res];
 			if (elem.color){
-				[canvas, context] = createCanvas(size, elem.color);
+				canvas = createCanvas(size, size);
+				addBackground(canvas, elem.color);
 				textures[res] = canvas;
 			}
 		}
+
 		textureManager.textures['naturalResources'] = textures;
+
 		// general
 		textures = [];
-		[canvas, context] = createCanvas(size, GLOBALS.backgroundColor);
-		addTriangle(context, size, "#0FF");
+
+		canvas = createCanvas(size, size);
+		addTriangle(canvas, size, "#0FF");
 		textures['city'] = canvas;
 
 		textureManager.textures['general'] = textures;
 	},
 }
 
-function createCanvas(size, back){
+function createCanvas(width, height){
 	var canvas		= document.createElement('canvas');
-	canvas.width		= size;
-	canvas.height		= size;
-	var context		= canvas.getContext('2d');
+	canvas.width		= width;
+	canvas.height		= height;
 
-	context.fillStyle	= back;
-	context.fillRect(0, 0, size, size);
+	return canvas;
+}
 
-	return [canvas, context];
+function addBackground(canvas, back){
+	var ctx = canvas.getContext('2d');
+	ctx.fillStyle = back;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 }
 
-function addTriangle(ctx, size, color){
+function addTriangle(canvas, size, color){
+	var ctx = canvas.getContext('2d');
 	ctx.fillStyle = color;
 	ctx.beginPath();
 	ctx.moveTo(0,0);
