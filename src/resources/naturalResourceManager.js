de1bbb25dbbebf12b865a1432513b77d367a9b18
Luis <lorgam@gmail.com>
2019-10-05 12:24:48 +0100

    Natural resource generation is now proportional to the number of tiles where the resource can be placed
    Added a baseResourceManager object

--- src/resources/naturalResourceManager.js
+++ src/resources/naturalResourceManager.js
@@ -1,5 +1,4 @@
-import GLOBALS		from '../globals/globals.js';
-import textureManager 	from '../neuron/textureManager.js';
+import baseResourceManager	from './baseResourceManager.js';
 
 const naturalResourceManager = {
 	resources : {
@@ -8,56 +7,10 @@ const naturalResourceManager = {
 	}
 }
 
+naturalResourceManager.generateResources = function(worldMap){
+	baseResourceManager.generateResource(worldMap, naturalResourceManager, 'naturalResources', 'MINERAL', ['grass','grassMountain','forest'], 0.005);
+	baseResourceManager.generateResource(worldMap, naturalResourceManager, 'naturalResources', 'GAS', ['deepOcean','ocean'], 0.0075);
+}
+
 export default naturalResourceManager;
 
-naturalResourceManager.generateResources = function(worldMap){
-	var mapContext	= worldMap.mapCanvas.getContext('2d');
-	var totalSize = worldMap.options.width * worldMap.options.height;
-
-	naturalResourceManager.generateMinerals(worldMap, mapContext, totalSize);
-	naturalResourceManager.generateGas(worldMap, mapContext, totalSize);
-}
-
-naturalResourceManager.generateMinerals = function(worldMap, mapContext, totalSize){
-	var x, y, tile, total;
-
-	total = ~~(totalSize  * 0.0015);
-
-	while (total > 0){
-		x = ~~(Math.random() * worldMap.options.width);
-		y = ~~(Math.random() * worldMap.options.height);
-
-		tile = worldMap.map[x][y];
-
-		if (tile.height > worldMap.options.waterHeight + 0.15){
-			//Can have minerals
-			if (tile.resource) continue;
-			total--;
-
-			tile.resource = naturalResourceManager.resources.MINERAL;
-
-			mapContext.drawImage(textureManager.textures['naturalResources']['MINERAL'], x * GLOBALS.maxTileSize(), y * GLOBALS.maxTileSize(), GLOBALS.maxTileSize(), GLOBALS.maxTileSize());
-		}
-	}
-}
-
-naturalResourceManager.generateGas = function(worldMap, mapContext, totalSize){
-	var x, y, tile, total;
-	total = ~~(totalSize  * 0.0005);
-
-	while (total > 0){
-		x = ~~(Math.random() * worldMap.options.width);
-		y = ~~(Math.random() * worldMap.options.height);
-
-		tile = worldMap.map[x][y];
-
-		if (tile.height < worldMap.options.waterHeight + 0.15){
-			//Can have gas
-			if (tile.resource) continue;
-			total--;
-			tile.resource = naturalResourceManager.resources.GAS;
-
-			mapContext.drawImage(textureManager.textures['naturalResources']['GAS'], x * GLOBALS.maxTileSize(), y * GLOBALS.maxTileSize(), GLOBALS.maxTileSize(), GLOBALS.maxTileSize());
-		}
-	}
-}
