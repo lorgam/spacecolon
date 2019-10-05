106d544597bf61c6342921e1f19111d0d23de3e0
Luis <lorgam@gmail.com>
2019-10-05 15:00:07 +0100

    Natural resources have their own proportions tha control how much terrain your resource will take

--- src/resources/naturalResourceManager.js
+++ src/resources/naturalResourceManager.js
@@ -2,10 +2,34 @@ import baseResourceManager	from './baseResourceManager.js';
 
 const naturalResourceManager = {
 	resources : {
-		MINERAL		: {id:1,color:"#DC444F",text:"mineral",default:100,terrain:['grass','grassMountain','forest']},
-		GAS		: {id:2,color:"#CB8F1E",text:"gas",default:10,terrain:['deepOcean','ocean']},
-		GRASSLIFE	: {id:3,color:"#5774AA",text:"hervibore",default:25,terrain:['grass','grassMountain','forest']},
-		SEALIFE		: {id:4,color:"#FFFFFF",text:"fish",default:50,terrain:['deepOcean','ocean']}
+		MINERAL		: {
+			color:"#DC444F",
+			text:"mineral",
+			default:100,
+			proportion:0.0075,
+			terrain:['grass','grassMountain','forest']
+		},
+		GAS		: {
+			color:"#CB8F1E",
+			text:"gas",
+			default:10,
+			proportion:0.005,
+			terrain:['deepOcean','ocean']
+		},
+		GRASSLIFE	: {
+			color:"#5774AA",
+			text:"hervibore",
+			default:25,
+			proportion:0.005,
+			terrain:['grass','grassMountain','forest']
+		},
+		SEALIFE		: {
+			color:"#FFFFFF",
+			text:"fish",
+			default:50,
+			proportion:0.0075,
+			terrain:['deepOcean','ocean']
+		}
 	}
 }
 
@@ -13,7 +37,7 @@ naturalResourceManager.generateResources = function(worldMap){
 	var name, res;
 	for (name in naturalResourceManager.resources){
 		res = naturalResourceManager.resources[name];
-		baseResourceManager.generateResource(worldMap, naturalResourceManager, 'naturalResources', name, res.terrain, 0.005);
+		baseResourceManager.generateResource(worldMap, naturalResourceManager, 'naturalResources', name, res.terrain, res.proportion);
 	}
 }
 
