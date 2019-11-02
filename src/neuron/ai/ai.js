248cc7524f337f0fe9028151c3bf0500f1a46801
Luis <lorgam@gmail.com>
2019-11-02 13:09:06 +0000

    Added ai objects

---
+++ src/neuron/ai/ai.js
@@ -0,0 +1,8 @@
+import path from	'./path.js';
+
+const AI = {
+	path : path
+}
+
+export default AI;
+
---
+++ src/neuron/ai/path.js
@@ -0,0 +1,8 @@
+import aStar from	'./path/aStar.js';
+
+const path = {
+	aStar : aStar
+};
+
+export default path;
+
