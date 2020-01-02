35ec010df18569c79e5ffce751d09859dc50729c
Luis A. Martín <lorgam@gmail.com>
2020-01-02 17:23:18 +0000

    Added a method to check paths

--- test/neuron/ai/path/aStar.test.js
+++ test/neuron/ai/path/aStar.test.js
@@ -12,30 +12,16 @@ describe('aStar function', function() {
 
 				let start = new MockMapPoint2d(0,0,map);
 				let goal = new MockMapPoint2d(2,2,map);
-
 				let path = aStar(start, goal, h).reverse();
 
-				expect(path).to.have.lengthOf(5);
-
-				expect(map[path[0].x][path[0].y]).to.equal(0);
-				expect(path[0].x).to.equal(0);
-				expect(path[0].y).to.equal(0);
-
-				expect(map[path[1].x][path[1].y]).to.equal(1);
-				expect(path[1].x).to.equal(0);
-				expect(path[1].y).to.equal(1);
-
-				expect(map[path[2].x][path[2].y]).to.equal(2);
-				expect(path[2].x).to.equal(0);
-				expect(path[2].y).to.equal(2);
-
-				expect(map[path[3].x][path[3].y]).to.equal(5);
-				expect(path[3].x).to.equal(1);
-				expect(path[3].y).to.equal(2);
-
-				expect(map[path[4].x][path[4].y]).to.equal(8);
-				expect(path[4].x).to.equal(2);
-				expect(path[4].y).to.equal(2);
+				let expected = [
+						{x:0,y:0},
+						{x:0,y:1},
+						{x:0,y:2},
+						{x:1,y:2},
+						{x:2,y:2}
+				];
+				pathChecker(expected, path);
 		});
 
 		it('is a simple path (0,0) => (2,0) => (2,2)',() => {
@@ -47,30 +33,16 @@ describe('aStar function', function() {
 
 				let start = new MockMapPoint2d(0,0,map);
 				let goal = new MockMapPoint2d(2,2,map);
-
 				let path = aStar(start, goal, h).reverse();
 
-				expect(path).to.have.lengthOf(5);
-
-				expect(map[path[0].x][path[0].y]).to.equal(0);
-				expect(path[0].x).to.equal(0);
-				expect(path[0].y).to.equal(0);
-
-				expect(map[path[1].x][path[1].y]).to.equal(1);
-				expect(path[1].x).to.equal(1);
-				expect(path[1].y).to.equal(0);
-
-				expect(map[path[2].x][path[2].y]).to.equal(2);
-				expect(path[2].x).to.equal(2);
-				expect(path[2].y).to.equal(0);
-
-				expect(map[path[3].x][path[3].y]).to.equal(5);
-				expect(path[3].x).to.equal(2);
-				expect(path[3].y).to.equal(1);
-
-				expect(map[path[4].x][path[4].y]).to.equal(8);
-				expect(path[4].x).to.equal(2);
-				expect(path[4].y).to.equal(2);
+				let expected = [
+						{x:0,y:0},
+						{x:1,y:0},
+						{x:2,y:0},
+						{x:2,y:1},
+						{x:2,y:2}
+				];
+				pathChecker(expected, path);
 		});
 
 		it('is a ladder movement',() => {
@@ -82,31 +54,25 @@ describe('aStar function', function() {
 
 				let start = new MockMapPoint2d(0,0,map);
 				let goal = new MockMapPoint2d(2,2,map);
-
 				let path = aStar(start, goal, h).reverse();
 
-				expect(path).to.have.lengthOf(5);
-
-				expect(map[path[0].x][path[0].y]).to.equal(0);
-				expect(path[0].x).to.equal(0);
-				expect(path[0].y).to.equal(0);
-
-				expect(map[path[1].x][path[1].y]).to.equal(1);
-				expect(path[1].x).to.equal(0);
-				expect(path[1].y).to.equal(1);
-
-				expect(map[path[2].x][path[2].y]).to.equal(2);
-				expect(path[2].x).to.equal(1);
-				expect(path[2].y).to.equal(1);
-
-				expect(map[path[3].x][path[3].y]).to.equal(5);
-				expect(path[3].x).to.equal(1);
-				expect(path[3].y).to.equal(2);
-
-				expect(map[path[4].x][path[4].y]).to.equal(8);
-				expect(path[4].x).to.equal(2);
-				expect(path[4].y).to.equal(2);
+				let expected = [
+						{x:0,y:0},
+						{x:0,y:1},
+						{x:1,y:1},
+						{x:1,y:2},
+						{x:2,y:2}
+				];
+				pathChecker(expected, path);
 		});
 });
 
 var h = (pos, g) => pos.distance(g);
+
+var pathChecker = (actual, expected) => {
+		expect(actual).to.have.lengthOf(expected.length);
+		for (let i = 0; i < expected.length; ++i) {
+				expect(actual[i].x).to.equal(expected[i].x);
+				expect(actual[i].y).to.equal(expected[i].y);
+		}
+}
