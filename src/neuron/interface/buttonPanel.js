5d182cd854cd5f0519f0ed354acf0d33a75403a0
Luis A. Martín <lorgam@gmail.com>
2020-02-01 12:42:05 +0000

    Added reset function to ButtonPanel and retabbed

--- src/neuron/interface/buttonPanel.js
+++ src/neuron/interface/buttonPanel.js
@@ -1,34 +1,37 @@
-import MenuControl	from './menuControl.js';
+import MenuControl from './menuControl.js';
 
 function ButtonPanel(control, size, vertical = false){
-	this.control = control;
-	this.size = size;
-	this.v = vertical;
-
-	this.panel = {
-		back : null,
-		btn : []
-	};
-
-	this.lastPos = (this.v ? this.control.top : this.control.left);
+  this.control = control;
+  this.size = size;
+  this.v = vertical;
+  this.reset();
 }
 
-ButtonPanel.prototype.addButton = function(btn){
-	var control;
-	if (this.v) control = new MenuControl(this.control.left, this.lastPos, this.control.width, this.size);
-	else control = new MenuControl(this.lastPos, this.control.top, this.size, this.control.height);
+ButtonPanel.prototype.reset = function() {
+  this.panel = {
+    back : null,
+    btn : []
+  };
 
-	btn.control = control;
-	this.lastPos += this.size;
-	this.panel.btn[this.panel.btn.length] = btn;
+  this.lastPos = (this.v ? this.control.top : this.control.left);
+};
+
+ButtonPanel.prototype.addButton = function(btn){
+  var control;
+  if (this.v) control = new MenuControl(this.control.left, this.lastPos, this.control.width, this.size);
+  else control = new MenuControl(this.lastPos, this.control.top, this.size, this.control.height);
+
+  btn.control = control;
+  this.lastPos += this.size;
+  this.panel.btn[this.panel.btn.length] = btn;
 }
 
 ButtonPanel.prototype.draw = function(){
-	this.panel.btn.forEach((e) => {e.draw();});
+  this.panel.btn.forEach((e) => {e.draw();});
 }
 
 ButtonPanel.prototype.isClicked = function(){
-	this.panel.btn.forEach((e) => {e.isClicked();});
+  this.panel.btn.forEach((e) => {e.isClicked();});
 }
 
 export default ButtonPanel;
