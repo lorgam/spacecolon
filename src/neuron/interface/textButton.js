1b4c16ee8be792f5e52b26bb4dd58416cc64aed5
Luis A. Martín <lorgam@gmail.com>
2020-02-01 12:51:13 +0000

    More retabbing

--- src/neuron/interface/textButton.js
+++ src/neuron/interface/textButton.js
@@ -1,12 +1,12 @@
-import BaseButton	from './baseButton.js';
-import GLOBALS		from '../../globals/globals.js';
-import texts		from '../../globals/texts.js';
+import BaseButton from './baseButton.js';
+import GLOBALS from '../../globals/globals.js';
+import texts from '../../globals/texts.js';
 
 function TextButton(parent, control, backColor, section, text, textColor, click){
-	BaseButton.call(this, parent, control, backColor, click);
-	this.section = section;
-	this.text = text;
-	this.textColor = textColor;
+  BaseButton.call(this, parent, control, backColor, click);
+  this.section = section;
+  this.text = text;
+  this.textColor = textColor;
 }
 
 TextButton.prototype = Object.create(BaseButton.prototype);
@@ -14,21 +14,21 @@ TextButton.prototype = Object.create(BaseButton.prototype);
 TextButton.prototype.getText = function(){return texts.getText(this.section, this.text);}
 
 TextButton.prototype.drawText = function(){
-	this.ctx.fillStyle	= this.textColor;
-	this.ctx.font		= GLOBALS.buttonFont;
+  this.ctx.fillStyle  = this.textColor;
+  this.ctx.font   = GLOBALS.buttonFont;
 
-	var text = this.getText();
-	var textHeight = GLOBALS.fontHeight();
+  var text = this.getText();
+  var textHeight = GLOBALS.fontHeight();
 
-	var marginW = ~~((this.control.width - this.ctx.measureText(text).width) / 2);
-	var marginH = ~~((this.control.height - textHeight) / 2) + textHeight;
+  var marginW = ~~((this.control.width - this.ctx.measureText(text).width) / 2);
+  var marginH = ~~((this.control.height - textHeight) / 2) + textHeight;
 
-	this.ctx.fillText(text, this.control.left + marginW, this.control.top + marginH);
+  this.ctx.fillText(text, this.control.left + marginW, this.control.top + marginH);
 }
 
 TextButton.prototype.draw = function(){
-	BaseButton.prototype.draw.call(this);
-	this.drawText();
+  BaseButton.prototype.draw.call(this);
+  this.drawText();
 }
 
 export default TextButton;
--- src/world_map/city/city.js
+++ src/world_map/city/city.js
@@ -1,69 +1,69 @@
-import GLOBALS		from '../../globals/globals.js';
-import INPUT		from '../../globals/input.js';
-import texts		from '../../globals/texts.js';
-import unitManager	from '../../resources/unit/unitManager.js';
-import BaseState	from '../../neuron/baseState.js';
-import MenuControl	from '../../neuron/interface/menuControl.js';
-import TextButton	from '../../neuron/interface/textButton.js';
-import ButtonPanel	from '../../neuron/interface/buttonPanel.js';
-import buildingManager	from './buildingManager.js';
+import GLOBALS  from '../../globals/globals.js';
+import INPUT  from '../../globals/input.js';
+import texts  from '../../globals/texts.js';
+import unitManager from '../../resources/unit/unitManager.js';
+import BaseState from '../../neuron/baseState.js';
+import MenuControl from '../../neuron/interface/menuControl.js';
+import TextButton from '../../neuron/interface/textButton.js';
+import ButtonPanel from '../../neuron/interface/buttonPanel.js';
+import buildingManager from './buildingManager.js';
 
 function City(parent){
-	BaseState.call(this);
-	this.parent = parent;
-	this.context = GLOBALS.context;
-	// buttons
-	var ctrl, btn;
-	var topBackBtn = GLOBALS.bottomOfMap() - GLOBALS.verticalButtonSize;
-	var top = GLOBALS.topMenuHeight;
+  BaseState.call(this);
+  this.parent = parent;
+  this.context = GLOBALS.context;
+  // buttons
+  var ctrl, btn;
+  var topBackBtn = GLOBALS.bottomOfMap() - GLOBALS.verticalButtonSize;
+  var top = GLOBALS.topMenuHeight;
 
-	// back
-	ctrl = new MenuControl(GLOBALS.mainScreenWidth, topBackBtn, GLOBALS.rightMenuSize(), GLOBALS.verticalButtonSize);
-	btn = new TextButton(this, ctrl, "#008", "general", "back", GLOBALS.highlightColor, function(){this.parent.nextState = this.parent.parent.parent;});
-	this.btnBack = btn;
+  // back
+  ctrl = new MenuControl(GLOBALS.mainScreenWidth, topBackBtn, GLOBALS.rightMenuSize(), GLOBALS.verticalButtonSize);
+  btn = new TextButton(this, ctrl, "#008", "general", "back", GLOBALS.highlightColor, function(){this.parent.nextState = this.parent.parent.parent;});
+  this.btnBack = btn;
 
-	// right panel
-	ctrl = new MenuControl(GLOBALS.mainScreenWidth, top, GLOBALS.rightMenuSize(), top - topBackBtn);
-	this.rigthPanel = new ButtonPanel(ctrl, GLOBALS.verticalButtonSize, true);
+  // right panel
+  ctrl = new MenuControl(GLOBALS.mainScreenWidth, top, GLOBALS.rightMenuSize(), top - topBackBtn);
+  this.rigthPanel = new ButtonPanel(ctrl, GLOBALS.verticalButtonSize, true);
 
-	for (var building in buildingManager.buildings){
-		btn = new TextButton(this, null, "#008", "buildings", building, GLOBALS.highlightColor, buildingClick);
-		this.rigthPanel.addButton(btn);
-	}
+  for (var building in buildingManager.buildings){
+    btn = new TextButton(this, null, "#008", "buildings", building, GLOBALS.highlightColor, buildingClick);
+    this.rigthPanel.addButton(btn);
+  }
 
-	// city resources
-	unitManager.addRobot(this);
+  // city resources
+  unitManager.addRobot(this);
 }
 
 City.prototype = Object.create(BaseState.prototype);
 
 City.prototype.text = function() {
-	return "city";
+  return "city";
 }
 
 City.prototype.update = function() {
-	if (INPUT.mouse.clicked){
-		this.btnBack.isClicked();
-		this.rigthPanel.isClicked();
-	}
+  if (INPUT.mouse.clicked){
+    this.btnBack.isClicked();
+    this.rigthPanel.isClicked();
+  }
 }
 
 City.prototype.draw = function() {
-	this.drawBackground();
-	this.btnBack.draw();
-	this.rigthPanel.draw();
+  this.drawBackground();
+  this.btnBack.draw();
+  this.rigthPanel.draw();
 }
 
 City.prototype.drawBackground = function() {
-	this.parent.parent.draw();
-	this.context.globalAlpha = 0.5;
-	this.context.fillStyle = "#008";
-	this.context.fillRect(0, GLOBALS.topMenuHeight, GLOBALS.mainScreenWidth, GLOBALS.mainScreenHeight);
-	this.context.globalAlpha = 1;
+  this.parent.parent.draw();
+  this.context.globalAlpha = 0.5;
+  this.context.fillStyle = "#008";
+  this.context.fillRect(0, GLOBALS.topMenuHeight, GLOBALS.mainScreenWidth, GLOBALS.mainScreenHeight);
+  this.context.globalAlpha = 1;
 }
 
 function buildingClick(number) {
-	var building = buildingManager.buildings[this.text];
+  var building = buildingManager.buildings[this.text];
 }
 
 export default City;
