8e27468b8e8ee816cefb2eb60bc92011240a26f9
Luis A. Martín <lorgam@gmail.com>
2020-02-01 12:36:49 +0000

    Retabbed text file and added move, fortify and cancel items

--- src/globals/texts.js
+++ src/globals/texts.js
@@ -1,73 +1,80 @@
 const texts = {
-	language	: 'en',
+  language : 'en',
 
-	en : {
-		mainMenu : {
-			start		: "Start",
-			options		: "Options",
-			exit		: "Exit"
-		},
-		optionsMenu : {
-			language	: "Language",
-			tileSize	: "Tile Size",
-			little		: "Little",
-			medium		: "Medium",
-			big		: "Big"
-		},
-		resources : {
-			mineral		: "mineral",
-			gas		: "gas",
-			fish		: "fish",
-			hervibore	: "hervibore",
-		},
-		buildings : {
-			ROBOT_FACTORY	: "Robot factory",
-			FISH_TANK	: "Fish tank",
-			MINE		: "Mine",
-		},
-		general : {
-			back		: "Back",
-			nextTurn	: "Next turn",
-		},
-		units : {
-			robots		: "Robot"
-		}
-	},
-	es : {
-		mainMenu : {
-			start		: "Empezar",
-			options		: "Opciones",
-			exit		: "Salir"
-		},
-		optionsMenu : {
-			language	: "Idioma",
-			tileSize	: "Tamaño de cuadricula",
-			little		: "Pequeño",
-			medium		: "Mediano",
-			big		: "Grande"
-		},
-		resources : {
-			mineral		: "mineral",
-			gas		: "gas",
-			fish		: "peces",
-			hervibore	: "hervíboros",
-		},
-		buildings : {
-			ROBOT_FACTORY	: "Fábrica de robots",
-			FISH_TANK	: "Tanque de pescado",
-			MINE		: "Mina",
-		},
-		general : {
-			back		: "volver",
-			nextTurn	: "Siguiente turno",
-		},
-		units : {
-			robots		: "Robot"
-		}
+  en : {
+    mainMenu : {
+      start : "Start",
+      options : "Options",
+      exit : "Exit"
+    },
+    optionsMenu : {
+      language : "Language",
+      tileSize : "Tile Size",
+      little : "Little",
+      medium : "Medium",
+      big : "Big"
+    },
+    resources : {
+      mineral : "mineral",
+      gas : "gas",
+      fish : "fish",
+      hervibore : "hervibore",
+    },
+    buildings : {
+      ROBOT_FACTORY : "Robot factory",
+      FISH_TANK : "Fish tank",
+      MINE : "Mine",
+    },
+    general : {
+      back : "Back",
+      nextTurn : "Next turn",
+      move : "Move",
+      fortify : "Fortify",
+      cancel : "Cancel",
+    },
+    units : {
+      robots : "Robot"
+    }
+  },
+  es : {
+    mainMenu : {
+      start : "Empezar",
+      options : "Opciones",
+      exit : "Salir"
+    },
+    optionsMenu : {
+      language : "Idioma",
+      tileSize : "Tamaño de cuadricula",
+      little : "Pequeño",
+      medium : "Mediano",
+      big : "Grande"
+    },
+    resources : {
+      mineral : "mineral",
+      gas : "gas",
+      fish : "peces",
+      hervibore : "hervíboros",
+    },
+    buildings : {
+      ROBOT_FACTORY : "Fábrica de robots",
+      FISH_TANK : "Tanque de pescado",
+      MINE : "Mina",
+    },
+    general : {
+      back : "volver",
+      nextTurn : "Siguiente turno",
+      move : "Mover",
+      fortify : "Fortificar",
+      cancel : "Cancelar",
+    },
+    units : {
+      robots : "Robot"
+    }
 
-	}
+  }
 }
 
 texts.getText = function(section, text){return texts[texts.language][section][text];}
 
 export default texts;
+
