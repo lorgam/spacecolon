c809df92caea705cbfabb4fffb93f03eaf8038ee
Luis A. Martín <lorgam@gmail.com>
2020-02-01 12:40:04 +0000

    Added select event to basestate and retabbed

--- src/neuron/baseState.js
+++ src/neuron/baseState.js
@@ -1,19 +1,25 @@
 import INPUT from '../globals/input.js';
 
 function BaseState(){
-	this.nextState = null;
+  this.nextState = null;
 }
 
 BaseState.prototype.changeState = function(screen) {
-	if (this.nextState != null){ //Change the state
-		var aux = this.nextState;
-		this.nextState = null;
-		screen.currentState = aux;
-		INPUT.resetKeyboard();
-	}
+  if (this.nextState != null){ //Change the state
+    //Events
+    this.nextState.select();
+    //Swap the state
+    var aux = this.nextState;
+    this.nextState = null;
+    screen.currentState = aux;
+    //Clean inputs
+    INPUT.resetKeyboard();
+  }
 };
 
 BaseState.prototype.update = function() {};
 BaseState.prototype.draw = function() {};
+BaseState.prototype.select = function() {};
 
 export default BaseState;
+
